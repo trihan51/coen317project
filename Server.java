52b4036e640a644ce9e0ddeb66135860be193173
EC2 Default User <ec2-user@ip-172-31-29-71.us-west-2.compute.internal>
2021-01-31 07:59:36 +0000

    added basic HTTP functionality

--- Server.java
+++ Server.java
@@ -1,9 +1,32 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class Server {
     public static void main(String[] args) {
-        int port = 8053;
+
+        int port = 8080;
+        String document_root = "";
+
+        // Processing Command Line Arguments (if any)
+        if (args.length == 0) {
+            System.out.println("no args");
+        } else if (args.length % 2 == 0) {
+            for (int i = 0; i < args.length; i++) {
+                switch (args[i]) {
+                    case "-document_root":
+                        document_root=args[i+1];
+                        break;
+                    case "-port":
+                        port = Integer.parseInt(args[i+1]);
+                        break;
+                    default:
+                }
+            }
+        } else {
+            System.out.println("Invalid arguments");
+            System.exit(0);
+        }
 
         try {
             ServerSocket serverSocket = new ServerSocket(port);
@@ -13,10 +36,36 @@ public class Server {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
             String inputLine, outputLine;
+            StringBuilder request = new StringBuilder();
             while ((inputLine = in.readLine()) != null) {
+                request.append(inputLine).append("\r\n");
                 System.out.println(inputLine);
-                System.out.println("new line");
+                if (inputLine.isEmpty()) {
+                    break;
+                }
             }
+            System.out.println("request is handled here");
+
+            StringBuilder contentBuilder = new StringBuilder();
+            try {
+                BufferedReader in2 = new BufferedReader(new FileReader("SCU_HOME.html"));
+                String s;
+                while ((s = in2.readLine()) != null) {
+                    System.out.println(s);
+                    contentBuilder.append(s);
+                }
+                in2.close();
+            } catch (IOException e) {
+                System.out.println("error");
+            }
+
+            String response = contentBuilder.toString();
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-Type: text/html");
+            out.println("Content-Length: " + response.length());
+            out.println("Date: " + new Date());
+            out.println("\r\n");
+            out.println(response);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
